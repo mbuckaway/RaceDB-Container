77b9bb1e9f80b1132cb5ea168367aa3099b8b780
Mark Buckaway <mark@buckaway.ca>
2019-12-08 22:30:37 -0500

    Fixed some issues

--- buildwin.ps1
+++ buildwin.ps1
@@ -4,7 +4,7 @@ if (Test-Path release -PathType leaf)
 	rmdir -Recurse -Force release
 }
 mkdir release
-ps2exe -inputFile .\windows\RaceDBController.Export.ps1 -outputFile .\release\RaceDBController.exe -x64 -noConsole -title "RaceDB Controller" -version 1.0.0 -noOutput -noError
+ps2exe -inputFile .\windows\RaceDBController.Export.ps1 -outputFile .\release\RaceDBController.exe -x64 -noConsole -title "RaceDB Controller" -version 1.0.0 -noOutput -noError -iconFile windows\RaceDB.ico
 copy racedb.env release\
 copy docker-compose*.yml release\
 copy dbconfig.env.tmpl release\
